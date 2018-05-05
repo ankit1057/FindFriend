378a12c090b4c3d146a8df5a6e97bb7d18a62d11
pichsinee <p.pichsinee@gmail.com>
2018-05-05 11:55:39 +0700

    Toolbar OK

--- app/src/main/java/sdu/cs/pichsinee/findfriend/fragment/RegisterFragment.java
+++ app/src/main/java/sdu/cs/pichsinee/findfriend/fragment/RegisterFragment.java
@@ -3,13 +3,44 @@ package sdu.cs.pichsinee.findfriend.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import sdu.cs.pichsinee.findfriend.MainActivity;
 import sdu.cs.pichsinee.findfriend.R;
 
 public class RegisterFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }//end Main Method (onActivityCreated)
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.new_register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill All Blank");
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }//end Method createToolbar
 
     @Nullable
     @Override
